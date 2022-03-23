52c49c999fdb99d7b83d42442ec461a954663e10
236538 Graczyk Kamil <236538@edu.p.lodz.pl>
2022-03-23 14:58:50 +0100

    Create simple Calculator class with multiplication and division

---
+++ Wspolbiezne/Etap0/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Example
+{
+    public class Calculator
+    {
+        public int Multiply(int a, int b) {
+            return a * b;
+        }
+
+        public int Divide(int a, int b) {
+            if (b == 0) { 
+                throw new System.DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
