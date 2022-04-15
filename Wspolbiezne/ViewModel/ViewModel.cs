133bd36aa961771673007306ded0c9fb253a5092
St0n3k <kamillokoks@gmail.com>
2022-04-15 22:13:23 +0200

    Changed View project type. Connected View layer with ViewModel layer

--- Wspolbiezne/ViewModel/ViewModel.cs
+++ Wspolbiezne/ViewModel/ViewModel.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Windows.Input;
 
 namespace ViewModel
 {
-    public class ViewModel
+    public class ViewModelController
     {
+        public ViewModelController() {
+            HelloCommand = new RelayCommand(Hello);
+        }
+        private int ballNumber = 1;
 
+        public int BallNumber { get => ballNumber; set => ballNumber=value; }
+        public ICommand HelloCommand { get ; set; }
+
+        private ICommand helloCommand;
+
+        private void Hello() {
+            System.Diagnostics.Debug.WriteLine("test");
+        }
     }
 }
